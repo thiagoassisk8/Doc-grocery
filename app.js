bd2e15699257e69077799a91d5ace23eea57b7e6
thiagoassisk8 <tyagosilva82@gmail.com>
2024-04-23 14:40:33 -0300

    few tweaks

--- app.js
+++ app.js
@@ -3,9 +3,8 @@ const app = express();
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
-
+app.use('/api-docs', swaggerUi.serve);
+app.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
